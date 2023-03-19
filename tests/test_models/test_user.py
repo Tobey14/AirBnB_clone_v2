1ad8a38ebf29055d856599ce603ca65f3e9032b7
Onuigbo Tobechukwu Collins <sureboytobi@gmail.com>
2023-03-19 22:10:38 +0100

    User test fix

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,7 +2,6 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
-import os
 
 
 class test_User(test_basemodel):
@@ -17,27 +16,21 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ testing user first anme attr"""
         new = self.value()
-        self.assertEqual(type(new.first_name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ testing user last name attr"""
         new = self.value()
-        self.assertEqual(type(new.last_name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ testing user email attr"""
+        def test_email(self):
+        """ """
         new = self.value()
-        self.assertEqual(type(new.email), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ testing user password attr"""
         new = self.value()
-        self.assertEqual(type(new.password), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(type(new.password), str)    
