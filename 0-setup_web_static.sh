af209c880881a4a7297687fe5dea76e6a7989992
tobechukwu onuigbo <tobechukwu.onuigbo@sterling.ng>
2023-04-06 23:20:19 +0100

    Do deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,26 @@
 # Sets up your web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "Fake index.html web-01" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    ALX School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "40i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
